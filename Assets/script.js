291df72219c13274c5b3c18935502a15d9c49d85
HotBarbeque <122079759+HotBarbeque@users.noreply.github.com>
2023-03-19 04:22:26 -0500

    Added Timer function to JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 var startButton = document.getElementById('start')
 var paragraph = document.getElementById('intro')
+var timeEl = document.querySelector("#time");
 
 //starting the quiz
 startButton.addEventListener('click', function quizStart() {
@@ -8,3 +9,22 @@ startButton.addEventListener('click', function quizStart() {
 });
 
 //timer
+var secondsLeft = 60;
+startButton.addEventListener('click', function setTime() {
+    var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent  = "Time:" + secondsLeft;
+
+    if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+    }
+    }, 1000);
+})
+
+function sendMessage() {
+    timeEl.textContent = " ";
+
+}
+
+setTime();
